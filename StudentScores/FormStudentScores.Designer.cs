b568d1b73cb2bb6f24fea1c8422e936097ffb550
LeeTaylor-git <71027054+LeeTaylor-git@users.noreply.github.com>
2023-11-29 22:55:10 -0700

    Commit all changes.

--- StudentScores/FormStudentScores.Designer.cs
+++ StudentScores/FormStudentScores.Designer.cs
@@ -28,20 +28,36 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
-			// Form1
+			// label1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 15);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// FormStudentScores
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(464, 257);
-			this.Name = "Form1";
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(351, 208);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.Name = "FormStudentScores";
+			this.Text = "Student Scores";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
 	}
 }
 

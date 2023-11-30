181ee975168c57cd700d556c15bae00929d4fa4f
LeeTaylor-git <71027054+LeeTaylor-git@users.noreply.github.com>
2023-11-29 23:37:26 -0700

    Completed new student form design.

--- StudentScores/frmAddNewStudent.Designer.cs
+++ StudentScores/frmAddNewStudent.Designer.cs
@@ -28,22 +28,157 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtScore = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.lblScores = new System.Windows.Forms.Label();
+			this.btbAdd = new System.Windows.Forms.Button();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.btnOK = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 13);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(42, 15);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Name:";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(53, 12);
+			this.txtName.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(233, 23);
+			this.txtName.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 54);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(39, 15);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Score:";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// txtScore
+			// 
+			this.txtScore.Location = new System.Drawing.Point(53, 54);
+			this.txtScore.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.txtScore.Name = "txtScore";
+			this.txtScore.Size = new System.Drawing.Size(150, 23);
+			this.txtScore.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(9, 101);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(44, 15);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Scores:";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// lblScores
+			// 
+			this.lblScores.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.lblScores.Location = new System.Drawing.Point(59, 95);
+			this.lblScores.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblScores.Name = "lblScores";
+			this.lblScores.Size = new System.Drawing.Size(227, 27);
+			this.lblScores.TabIndex = 5;
+			this.lblScores.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// btbAdd
+			// 
+			this.btbAdd.Location = new System.Drawing.Point(211, 53);
+			this.btbAdd.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.btbAdd.Name = "btbAdd";
+			this.btbAdd.Size = new System.Drawing.Size(75, 23);
+			this.btbAdd.TabIndex = 6;
+			this.btbAdd.Text = "&Add Score";
+			this.btbAdd.UseVisualStyleBackColor = true;
+			// 
+			// btnClear
+			// 
+			this.btnClear.Location = new System.Drawing.Point(204, 124);
+			this.btnClear.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(82, 23);
+			this.btnClear.TabIndex = 7;
+			this.btnClear.Text = "Clear &Scores";
+			this.btnClear.UseVisualStyleBackColor = true;
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnCancel.Location = new System.Drawing.Point(204, 153);
+			this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(82, 23);
+			this.btnCancel.TabIndex = 8;
+			this.btnCancel.Text = "&Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			// 
+			// btnOK
+			// 
+			this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.btnOK.Location = new System.Drawing.Point(114, 153);
+			this.btnOK.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(82, 23);
+			this.btnOK.TabIndex = 9;
+			this.btnOK.Text = "&OK";
+			this.btnOK.UseVisualStyleBackColor = true;
 			// 
 			// frmAddNewStudent
 			// 
+			this.AcceptButton = this.btnOK;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.btnCancel;
 			this.ClientSize = new System.Drawing.Size(300, 179);
+			this.Controls.Add(this.btnOK);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.btbAdd);
+			this.Controls.Add(this.lblScores);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtScore);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.label1);
 			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
 			this.Name = "frmAddNewStudent";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Add New Student";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtName;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtScore;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label lblScores;
+		private System.Windows.Forms.Button btbAdd;
+		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.Button btnOK;
 	}
 }
